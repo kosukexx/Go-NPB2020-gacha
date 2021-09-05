2801767969f5d29c6d19f8d57508ee700e54ec23
Kosuke Yamashita <kosuke.yamashita.3b@stu.hosei.ac.jp>
2021-09-06 02:53:27 +0900

    Webページとくっつけて動くようにした

--- main.go
+++ main.go
@@ -1,8 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("This is Go-NPB2020-gacha.")
+func main() {
+
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.html")
+
+
+	r.GET("/", func(c *gin.Context){
+		c.HTML(http.StatusOK, "index.html", gin.H{"title":"プロ野球有名選手ガチャ","message":"何回引く？"})
+	})
+	r.POST("",func(c *gin.Context){
+		number:=c.PostForm("field")
+		c.HTML(http.StatusOK, "index.html", gin.H{"title":"プロ野球有名選手ガチャ","message":result(number)})
+
+	})
+
+	/*
+	r.GET("", func(c *gin.Context) {
+		c.JSON(200,gin.H{
+			"name":"nakata",
+		})
+	})
+
+	 */
+	r.Run(":8080")
 
 }
+func result(number string) string{
+	//numberInt,_ :=strconv.Atoi(number)
+	message:=number
+
+
+	return message;
+}
+/*
+func handler(c *gin.Context) {
+	c.HTML(200, "index.html", gin.H{})
+}
+
+ */
